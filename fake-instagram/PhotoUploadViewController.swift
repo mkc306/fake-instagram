9b59c2080465b302f8fe01c03a58ad64796a7987
Jeremy Ong <jeremy.ongws@gmail.com>
2016-04-22 17:50:24 +0800

    temp

--- fake-instagram/PhotoUploadViewController.swift
+++ fake-instagram/PhotoUploadViewController.swift
@@ -11,33 +11,54 @@ import FastttCamera
 
 class PhotoUploadViewController: UIViewController, FastttCameraDelegate {
 	
+	@IBOutlet weak var cameraView: UIView!
 	let fastCamera = FastttCamera()
 	
 	
-    override func viewDidLoad() {
-			
-        super.viewDidLoad()
-			self.fastCamera.delegate = self
-			
-			self.fastttAddChildViewController(self.fastCamera)
-			self.fastCamera.view.frame = self.view.frame
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	override func viewDidLoad() {
+		
+		super.viewDidLoad()
+		self.fastCamera.delegate = self
+		
+		self.fastttAddChildViewController(self.fastCamera)
+		
+		self.fastCamera.view.frame = self.cameraView.frame
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func onTakePicButtonPressed(sender: UIBarButtonItem) {
+		print("take pic button pressed")
+		self.fastCamera.takePicture()
+	}
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+	//MARK: IFTTTFastttCameraDelegate
+	
+	func cameraController(cameraController: FastttCameraInterface!, didFinishCapturingImage capturedImage: FastttCapturedImage!) {
+		print("a photo was taken")
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
 }
