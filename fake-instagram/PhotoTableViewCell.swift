cf243beb16eaad28ad6311daf2d7aa707bebe83b
mkc306 <m3k0c6@gmail.com>
2016-04-22 16:19:10 +0800

    basic storyboard finished

---
+++ fake-instagram/PhotoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoTableViewCell.swift
+//  fake-instagram
+//
+//  Created by Michael Kelvyn Chan on 4/22/16.
+//  Copyright © 2016 Jeremy Ong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell{
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var CaptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
