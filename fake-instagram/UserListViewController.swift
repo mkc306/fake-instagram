dcc7801b526f0b0d7f5a28d5593399c49a026df8
mkc306 <m3k0c6@gmail.com>
2016-04-25 12:40:56 +0800

    w

--- fake-instagram/UserListViewController.swift
+++ fake-instagram/UserListViewController.swift
@@ -10,60 +10,38 @@ import UIKit
 
 class UserListViewController: UITableViewController {
 @IBOutlet weak var searchTextField: UITextField!
+var users = [User] ()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-  
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        DataService.dataService.USER_REF.observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+            if let value = snapshot.value as? [String:AnyObject] {
+                let user = User(key: snapshot.key, dict: value)
+                self.users.append(user)
+                self.tableView.reloadData()
+            }
+    })
     }
 
     // MARK: - Table view data source
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return users.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("UserCell", forIndexPath: indexPath)
-
-        // Configure the cell...
-        cell.textLabel?.text = "testcell"
+        let user = self.users[indexPath.row]
+        cell.textLabel?.text = user.username
         cell.layer.borderWidth = 0.8
         cell.layer.borderColor = UIColor.redColor().CGColor
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+  
+ 
 
     /*
     // Override to support rearranging the table view.
@@ -80,14 +58,16 @@ class UserListViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destinationViewController as? OtherProfileViewController {
+            let selected = tableView.indexPathForSelectedRow
+            let user = users[selected!.row]
+            destination.user = user
+        }
+       
     }
-    */
+    
 
 }
