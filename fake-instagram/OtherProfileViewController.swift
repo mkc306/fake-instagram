35344c60014e71afc20503c964862460a365df59
wilsonchuah <wilsonchuah93@gmail.com>
2016-04-25 16:34:21 +0800

    complete follower following

--- fake-instagram/OtherProfileViewController.swift
+++ fake-instagram/OtherProfileViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class OtherProfileViewController: UIViewController {
+class OtherProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var user : User!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var isFollowing = false
     override func viewDidLoad() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         super.viewDidLoad()
-        user.key
+
         // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PhotoCell") as? PhotoTableViewCell
+        return cell!
     }
     
     
