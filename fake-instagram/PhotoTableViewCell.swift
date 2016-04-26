d941575398c4eaf4e51b3bc4cf55f33b06f1b19b
wilsonchuah <wilsonchuah93@gmail.com>
2016-04-26 15:48:10 +0800

    update

--- fake-instagram/PhotoTableViewCell.swift
+++ fake-instagram/PhotoTableViewCell.swift
@@ -17,24 +17,46 @@ class PhotoTableViewCell: UITableViewCell{
         let tap = UITapGestureRecognizer.init(target: self, action: "segueToPhoto" )
         addGestureRecognizer(tap)
         
+        if let image = imageView?.image{
+            
+            let likedSignAnimated: Selector = "likeSignTurnRed"
+            let doubleTapGesture = UITapGestureRecognizer(target: self.imageView, action:likedSignAnimated )
+            doubleTapGesture.numberOfTapsRequired = 2
+            
+            
+            
+            
+            if let userId = NSUserDefaults.standardUserDefaults().valueForKey("uid") as? String {let dict = ["liked":liked, "userId":userId]
+            
+            DataService.dataService.PHOTO_REF.childByAutoId().setValue(dict, andPriority: nil, withCompletionBlock: { (error, ref) -> Void in
+                
+                DataService.dataService.USER_REF.childByAppendingPath(userId).childByAppendingPath("liked").updateChildValues(ref.key: true)
+                
+            })
+        }
     }
     
     
     
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        override func setSelected(selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+            
+            // Configure the view for the selected state
+        }
         
-        // Configure the view for the selected state
+        func segueToPhoto(){
+            let view = self.window?.rootViewController?.presentedViewController
+            let storyboard = UIStoryboard(name: "FeedViews", bundle: nil)
+            let nextViewController = storyboard.instantiateViewControllerWithIdentifier("PhotoViewController")
+            view?.presentViewController(nextViewController, animated: true, completion: nil)
+        }
+        
     }
     
-    func segueToPhoto(){
-        let view = self.window?.rootViewController?.presentedViewController
-        let storyboard = UIStoryboard(name: "FeedViews", bundle: nil)
-        let nextViewController = storyboard.instantiateViewControllerWithIdentifier("PhotoViewController")
-        view?.presentViewController(nextViewController, animated: true, completion: nil)
-    }
-        
-        
-        
     
+}
+
+
+ func likeSignTurnRed(<#parameters#>) -> <#return type#> {
+    <#function body#>
 }
